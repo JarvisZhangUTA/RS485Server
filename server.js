14cf92fe0fb9fdb4e322ca8ca4155f2c02b18636
JarvisZhangUTA <zhangjw.uta@gmail.com>
2018-09-26 00:00:35 -0500

    test

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8080 });
+const fs = require('fs');
 
 wss.on('connection', function connection(ws) {
-    console.log(ws);
-
+    console.log('connection');
     ws.on('message', function incoming(message) {
         console.log('received: %s', message);
-        ws.send(message);
+        // ws.send(message);
+        fs.appendFile('message.txt', message);
     });
 });
