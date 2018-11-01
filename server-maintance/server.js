cb4f6e597b359e0fa52a1e148d635b92d1ec89f4
JarvisZhangUTA <zhangjw.uta@gmail.com>
2018-10-31 22:10:06 -0500

    test

--- server-maintance/server.js
+++ server-maintance/server.js
@@ -34,14 +34,14 @@ app.use(function (req, res, next) {
 const httpServer = http.createServer(app);
 const httpsServer = https.createServer(credentials, app);
 
-// httpServer.listen(80, () => { 
-//     console.log('HTTP Server listening on port 80') 
-// });
-
-// httpsServer.listen(443, () => {
-// 	console.log('HTTPS Server running on port 443');
-// });
-
-httpServer.listen(3000, () => { 
-		console.log('HTTP Server listening on port 3000') 
+httpServer.listen(80, () => { 
+    console.log('HTTP Server listening on port 80') 
 });
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});
+
+// httpServer.listen(3000, () => { 
+// 		console.log('HTTP Server listening on port 3000') 
+// });
